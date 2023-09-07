82c253df086483ad39ce82f79cc57430837a93eb
Max <7087907+maxpromer@users.noreply.github.com>
2023-09-07 22:51:05 +0700

    Initial commit

---
+++ libraries/EasyKids3in1Robot/examples/Digital/Wait_Start/Wait_Start.ino
@@ -0,0 +1,10 @@
+#include <EasyKids3in1.h>
+
+void setup() {
+  EasyKids_Setup();
+}
+
+void loop() {
+  waitForStart();
+  beep();
+}
---
+++ libraries/EasyKids3in1Robot/examples/Servo/ServoTest/ServoTest.ino
@@ -0,0 +1,8 @@
+#include <EasyKids3in1.h>
+void setup() {
+  EasyKids_Setup();
+}
+
+void loop() {
+  testServo(1);  //Pin Number
+}
